c7f1b06956e6d7e5b3c973caafdb80021e974ddb
Adiel Felsen <adielfelsen@Adiels-MacBook-Pro.local>
2018-11-08 21:44:53 -0500

    Created Pacman, Powerup and Pinky sprites, basic controller, added images.

--- Pacman.py
+++ Pacman.py
@@ -1,26 +1,32 @@
-class Pacman:
+import pygame
+
+class Pacman(pygame.sprite.Sprite):
     def __init__(self, filename, x, y, speed):
-        self.x = x
-        self.y = y
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(pygame.image.load(filename),(20,20))
+        self.rect = self.image.get_rect()
+
+        self.rect.x = x
+        self.rect.y = y
         self.speed = speed
     def __str__(self):
-        string = "(x:" + str(self.x) + ", y:" + str(self.y) + ")" + " Speed" + self.speed
+        string = "(x:" + str(self.rect.x) + ", y:" + str(self.rect.y) + ")" + " Speed" + self.speed
         return string
     def moveLeft(self):
-        self.x -= self.speed
-        if self.x < 0:
-            self.x = 0
+        self.rect.x -= self.speed
+        if self.rect.x < 0:
+            self.rect.x = 0
     def moveRight(self):
-        self.x += self.speed
-        if self.x < 0:
-            self.x = 0
+        self.rect.x += self.speed
+        if self.rect.x > 640-20:
+            self.rect.x = 640-20
     def moveDown(self):
-        self.y += self.speed
-        if self.y < 0:
-            self.y = 0
+        self.rect.y += self.speed
+        if self.rect.y > 480-20:
+            self.rect.y = 480-20
     def moveUp(self):
-        self.y -= self.speed
-        if self.y < 0:
-            self.y = 0
+        self.rect.y -= self.speed
+        if self.rect.y < 0:
+            self.rect.y = 0
     def getCoordinates(self):
-        return self.x,self.y
+        return self.rect.x,self.rect.y
