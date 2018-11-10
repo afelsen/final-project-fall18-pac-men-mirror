a6f2ac105a1fef9bab32b353cf82c5a5f7c93f00
Matthew Damiata <matthewdamiata@Matts-MacBook-Pro-2.local>
2018-11-09 21:59:43 -0500

    updated menu

--- Controller.py
+++ Controller.py
@@ -2,50 +2,56 @@ import pygame
 import Pacman
 import Ghost
 import Powerup
+import random
 
 class Controller:
     def __init__(self, width = 640, height = 480):
         self.screen = pygame.display.set_mode((width,height))
         self.background = pygame.Surface(self.screen.get_size()).convert()
-        self.pacman = Pacman.Pacman('Pacman.png',100,100,20)
+        self.pacman = Pacman.Pacman('Pacman.png',0,0,20)
         self.pacGroup = pygame.sprite.Group(self.pacman)
-        self.pinky1 = Ghost.Pinky('pinky.png',100,100,20)
-        self.pinky2 = Ghost.Pinky('pinky.png',200,200,20)
+        self.pinky1 = Ghost.Pinky('pinky.png',random.randint(1,641),random.randint(1,481),20)
+        self.pinky2 = Ghost.Pinky('pinky.png',random.randint(1,641),random.randint(1,481),20)
         self.pinkyGroup = pygame.sprite.Group(self.pinky1,self.pinky2)
-        self.cherry = Powerup.Cherry('cherry.png',300,300)
+        self.cherry = Powerup.Cherry('cherry.png',random.randint(1,641),random.randint(1,481))
         self.cherryGroup = pygame.sprite.Group(self.cherry)
-        self.banana = Powerup.Banana('banana.png',400,400)
+        self.banana = Powerup.Banana('banana.png',random.randint(1,641),random.randint(1,481))
         self.bananaGroup = pygame.sprite.Group(self.banana)
-
 
         done = False
         introdone = False
         frame = 1
         self.pacDirection = ""
         self.pinkyangle = 45
-        self.pacangle = 0
         while not done:
             self.menuscreen = pygame.display.set_mode((width,height))
             self.menubackground = pygame.Surface(self.menuscreen.get_size()).convert()
             pygame.font.init()
-            self.myfont = pygame.font.Font("KaushanScript-Regular.otf", 50)
+            self.myfont = pygame.font.Font("KaushanScript-Regular.otf", 75)
             self.subfont = pygame.font.Font("KaushanScript-Regular.otf", 25)
-            frame = 1
+            self.midfont = pygame.font.Font("KaushanScript-Regular.otf", 50)
             while not introdone:
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         introdone = True
                         done = True
-                keys = pygame.key.get_pressed()
+                    keys = pygame.key.get_pressed()
+                    textTop = self.myfont.render("PacXon",False,(255,255,50))
+                    textBottom = self.subfont.render("Created by the PacMen",False,(255,255,50))
+                    textHelp = self.subfont.render("Press SPACEBAR to start!",False,(255,255,50))
+                    textControls = self.subfont.render("Controls: Arrow keys and Q to quit",False,(255,255,50))
                 if frame % 1 == 0:
-                    textTop = self.myfont.render("PacXon",False,(50,50,50))
-                    textBottom = self.subfont.render("Created by the PacMen",False,(50,50,50))
                     if keys[pygame.K_SPACE]: # space to start
                         introdone = True
+                    elif keys[pygame.K_q]: # q to quit
+                        introdone = True
+                        done = True
                     frame += 1
                     self.menuscreen.blit(self.menubackground,(0,0))
-                    self.menuscreen.blit(textTop,(242,100))
+                    self.menuscreen.blit(textTop,(205,75))
                     self.menuscreen.blit(textBottom,(200,300))
+                    self.menuscreen.blit(textHelp,(180,210))
+                    self.menuscreen.blit(textControls,(0,440))
                     pygame.display.flip()
 
             for event in pygame.event.get():
@@ -61,6 +67,8 @@ class Controller:
                     self.pacDirection = "L"
                 elif keys[pygame.K_RIGHT]:
                     self.pacDirection = "R"
+                elif keys[pygame.K_q]:
+                    done = True
 
                 if self.pacDirection == "U":
                     self.pacman.moveUp()
