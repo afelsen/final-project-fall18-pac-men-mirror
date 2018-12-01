ce1296707d5f0d49ac84789642e3db4511f11cbc
Matthew Damiata <matthewdamiata@Matts-MacBook-Pro-2.local>
2018-11-30 23:58:38 -0500

    small fix

--- src/Powerup.py
+++ src/Powerup.py
@@ -5,27 +5,16 @@ class Cherry(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.transform.scale(pygame.image.load(filename),(20,20))
         self.rect = self.image.get_rect()
-
         self.rect.x = x
         self.rect.y = y
     def getCoordinates(self):
         return self.rect.x,self.rect.y
-<<<<<<< HEAD
 
 class Banana(pygame.sprite.Sprite):
     def __init__(self, filename, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.transform.scale(pygame.image.load(filename),(20,20))
         self.rect = self.image.get_rect()
-=======
-
-class Banana(pygame.sprite.Sprite):
-    def __init__(self, filename, x, y):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.transform.scale(pygame.image.load(filename),(20,20))
-        self.rect = self.image.get_rect()
-
->>>>>>> ba2690bf87d3228fd4b1b432c545d077669b6aa2
         self.rect.x = x
         self.rect.y = y
     def getCoordinates(self):
@@ -36,7 +25,6 @@ class Snowflake(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.transform.scale(pygame.image.load(filename),(20,20))
         self.rect = self.image.get_rect()
-<<<<<<< HEAD
         self.rect.x = x
         self.rect.y = y
     def getCoordinates(self):
@@ -47,9 +35,6 @@ class Heart(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.transform.scale(pygame.image.load(filename),(20,20))
         self.rect = self.image.get_rect()
-=======
-
->>>>>>> ba2690bf87d3228fd4b1b432c545d077669b6aa2
         self.rect.x = x
         self.rect.y = y
     def getCoordinates(self):
