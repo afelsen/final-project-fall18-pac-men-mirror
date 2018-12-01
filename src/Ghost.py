4ac439b1821b373357aee4ebb612c679e4983f2d
Jack Stanley <jstanle2@binghamton.edu>
2018-11-30 20:05:48 -0500

    doc strings

--- src/Ghost.py
+++ src/Ghost.py
@@ -4,6 +4,10 @@ import random
 
 class Pinky(pygame.sprite.Sprite): #Pink; normal bouncing ghost
     def __init__(self, filename, x, y, speed):
+        '''
+        initializes the Pinky class
+        self, file name, x position, y position, and speed parameters
+        '''
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.transform.scale(pygame.image.load(filename),(20,20))
         self.rect = self.image.get_rect()
@@ -16,14 +20,28 @@ class Pinky(pygame.sprite.Sprite): #Pink; normal bouncing ghost
         self.ymultiplier = random.choice([-1,1])
 
     def update(self):
+        '''
+        updates the Pinky class's positon
+        self
+
+        '''
         self.rect.x += self.xmultiplier*self.speed/2
         self.rect.y += self.ymultiplier*self.speed/2
 
     def getCoordinates(self):
+        '''
+        gets the coordinates of the Pinky class
+        self
+        returns x and y position
+        '''
         return self.x,self.y
 
 class Inky(pygame.sprite.Sprite): #Light blue; Bounces in fenced in areas
     def __init__(self, filename, x, y, speed):
+        '''
+        initializes the Inky class
+        self, file name, x position, y position, and speed parameters
+        '''
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.transform.scale(pygame.image.load(filename),(20,20))
         self.rect = self.image.get_rect()
@@ -36,15 +54,29 @@ class Inky(pygame.sprite.Sprite): #Light blue; Bounces in fenced in areas
         self.ymultiplier = random.choice([-1,1])
 
     def update(self):
+        '''
+        updates the Inky class's positon
+        self
+
+        '''
         self.rect.x += self.xmultiplier*self.speed/2
         self.rect.y += self.ymultiplier*self.speed/2
 
 
     def getCoordinates(self):
+        '''
+        gets the coordinates of the Inky class
+        self
+        returns x and y position
+        '''
         return self.x,self.y
 
 class Blinky(pygame.sprite.Sprite): #Red; Breaks any block it hits - moves at 1/2 speed
     def __init__(self, filename, x, y, speed):
+        '''
+        initializes the Blinky class
+        self, file name, x position, y position, and speed parameters
+        '''
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.transform.scale(pygame.image.load(filename),(20,20))
         self.rect = self.image.get_rect()
@@ -57,15 +89,29 @@ class Blinky(pygame.sprite.Sprite): #Red; Breaks any block it hits - moves at 1/
         self.ymultiplier = random.choice([-1,1])
 
     def update(self):
+        '''
+        updates the Blinky class's positon
+        self
+
+        '''
         self.rect.x += self.xmultiplier*self.speed/2
         self.rect.y += self.ymultiplier*self.speed/2
 
 
     def getCoordinates(self):
+        '''
+        gets the coordinates of the Blinky class
+        self
+        returns x and y position
+        '''
         return self.x,self.y
 
 class Clyde(pygame.sprite.Sprite): #Orange; Follows the edges of the fences
     def __init__(self, filename, x, y, speed):
+        '''
+        initializes the Clyde class
+        self, file name, x position, y position, and speed parameters
+        '''
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.transform.scale(pygame.image.load(filename),(20,20))
         self.rect = self.image.get_rect()
@@ -77,6 +123,11 @@ class Clyde(pygame.sprite.Sprite): #Orange; Follows the edges of the fences
         self.ymultiplier = random.choice([-1,1])
 
     def update(self):
+        '''
+        updates the Clyde class's positon
+        self
+
+        '''
         if self.clydeState == 0:
             self.clydeState += 1
             self.rect.x += 20
@@ -96,4 +147,9 @@ class Clyde(pygame.sprite.Sprite): #Orange; Follows the edges of the fences
         print(self.rect.x)
 
     def getCoordinates(self):
+        '''
+        gets the coordinates of the Clyde class
+        self
+        returns x and y position
+        '''
         return self.x,self.y
